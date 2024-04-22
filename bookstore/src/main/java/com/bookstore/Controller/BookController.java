f83c4b8a5d14eb8bdffb3772be63f5ce5860e6d9
Shailesh kumar <skrshailesh1996@gmail.com>
2024-04-22 19:10:51 +0530

    MybookList, Mybook, service and repo added

--- bookstore/src/main/java/com/bookstore/Controller/BookController.java
+++ bookstore/src/main/java/com/bookstore/Controller/BookController.java
@@ -1,13 +1,23 @@
 package com.bookstore.Controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.*;
+import com.bookstore.entity.Book;
+import com.bookstore.service.BookService;
 
 
 
 @Controller
 public class BookController {
 
+	@Autowired
+	private BookService service;
 	
 	@GetMapping("/")
 	public String home() {
@@ -16,11 +26,28 @@ public class BookController {
 	
 	@GetMapping("/book_register")
 	public String bookRegister() {
-		return  "bookRegister";
+		return "bookRegister";
 	}
 	
 	@GetMapping("/available_books")
-	public String getAllBook() {
-		return  "bookList";
+//	public String getAllBook() {
+//		return  "bookList";}
+	public ModelAndView getAllBook() {
+		List<Book>list=service.getAllBook();
+		return new ModelAndView("bookList","book",list);
 	}
+	
+	
+	@PostMapping("/save")
+	public String addBook(@ModelAttribute Book b) {
+		service.save(b);  //this save(b) is called from class BookService
+		return "redirect:/available_books";
+	}
+	
+	@GetMapping("/my_books")
+	public String getMyBooks() {
+		return "myBooks";
+	}
+	
+	
 }
--- bookstore/src/main/java/com/bookstore/repository/BookRepository.java
+++ bookstore/src/main/java/com/bookstore/repository/BookRepository.java
@@ -1,5 +1,11 @@
 package com.bookstore.repository;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bookstore.entity.Book;
+
+@Repository  
+public interface BookRepository extends JpaRepository<Book, Integer>{
 
 }
