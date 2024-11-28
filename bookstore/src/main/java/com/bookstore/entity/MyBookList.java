ae23ac2c802d9bd0ba7c54e5ee710fed62610729
Shailesh kumar <skrshailesh1996@gmail.com>
2024-11-28 12:47:48 +0530

    Update MyBookList.java

--- bookstore/src/main/java/com/bookstore/entity/MyBookList.java
+++ bookstore/src/main/java/com/bookstore/entity/MyBookList.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 
 @Entity
-@Table(name="MyBooks")
+@Table(name = "MyBooks")
 public class MyBookList {
 
 	@Id
@@ -14,10 +14,12 @@ public class MyBookList {
 	private String name;
 	private String author;
 	private String price;
+
 	public MyBookList() {
 		super();
-		
+
 	}
+
 	public MyBookList(int id, String name, String author, String price) {
 		super();
 		this.id = id;
@@ -25,32 +27,37 @@ public class MyBookList {
 		this.author = author;
 		this.price = price;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+
 	public String getPrice() {
 		return price;
 	}
+
 	public void setPrice(String price) {
 		this.price = price;
 	}
-	
-	
-	
-	
+
 }
